263731378edbb590ec01580d2975c73bb5684ea0
morgankar <realrohankar03@gmail.com>
2025-05-17 12:18:56 +0530

    Second card done

--- components/Section3.jsx
+++ components/Section3.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Section3 = () => {
   return (
+    <>
     <div className="container max-w-[1200px] h-[100vh] m-auto">
         
             <h1 className='text-5xl font-bold leading-[50px] max-w-[32vw]'>Transforming Imagination into <span className='text-[#8b49e9] relative'>Reality</span></h1>
@@ -11,29 +12,42 @@ const Section3 = () => {
             {/* Row1 */}
             <div className="flex">
                 <div className=" rounded-2xl w-[468px] h-[324px] noise-bg overflow-hidden relative ">
-                  <div className="blck flex justify-around">
+                  <div className="blck flex justify-between">
                     <h6 className='text-sm ml-5 mt-5 w-90 leading-5 font-medium'>Dive into the world of AI where design possibilities are limitless. Let the cutting-edge technology transform your concepts into breathtaking visuals.</h6>
                     <div className="secIcon rounded-[50%] w-[50px] h-[50px] bg-[#8f4ded]  m-5 ">
-                    <svg className='pl-2 mt-1.5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="currentColor"><path d="M4 19H20V12H22V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V12H4V19ZM14 9V15H10V9H5L12 2L19 9H14Z"></path></svg>
+                    <svg className='pl-2 mt-1.5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="currentColor"><path d="M4.22173 18.3642L5.63574 19.7784L15.2427 10.1716L19.071 14L19.071 4.92892L9.99999 4.92893L13.8284 8.75738L4.22173 18.3642Z"></path></svg>
                   </div>
 
                   </div>
                   <h1 className='text-4xl absolute bottom-5 left-4 font-bold'>Witness The Future</h1>
                    
                 </div>
+                
                
-                <div className=" rounded-2xl w-[712px] h-[324px] noise-bg overflow-hidden  ml-4 relative"></div>
+                <div className=" rounded-2xl w-[712px] h-[324px] noise-bg overflow-hidden  ml-4 relative">
+                  <div className="blck flex justify-between">
+                    <h6 className='text-sm ml-5 mt-5 w-110 leading-5 font-medium'>Step beyond the ordinary with designs that defy conventions. Our AI conjures up imaginative visuals that push the boundaries of creativity.</h6>
+                    <div className="secIcon rounded-[50%] w-[50px] h-[50px] bg-[#8f4ded]  m-5 ">
+                    <svg className='pl-2 mt-1.5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="currentColor"><path d="M4.22173 18.3642L5.63574 19.7784L15.2427 10.1716L19.071 14L19.071 4.92892L9.99999 4.92893L13.8284 8.75738L4.22173 18.3642Z"></path></svg>
+                    </div>
+
+                  </div>
+                  <h1 className='text-4xl absolute bottom-5 left-4 font-bold'>Visualize the Impossible</h1>
+                </div>
+                </div>
             </div>
            
              {/* Row2 */}
-            <div className="flex ">
+            <div className="flex mt-5">
                 <div className="rounded-2xl w-[712px] h-[324px] noise-bg overflow-hidden  mr-5"></div>
                 <div className="rounded-2xl w-[468px] h-[324px] noise-bg overflow-hidden  "></div>   
             </div>
             
-        </div>
     </div>
-  )
+        
+    
+  </>
+)
 }
 
 export default Section3
