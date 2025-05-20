500db3a3c6776621e7d050a89ebf80d0852f3b14
morgankar <realrohankar03@gmail.com>
2025-05-20 23:04:10 +0530

    wip

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -8,13 +8,15 @@ const Reviews = () => {
          <h1 className='w-[650px] mt-3 ml-9'>Discover how our platform has helped businesses create outstanding content effortlessly. Hear directly from our users about their success and satisfaction.</h1>
     </div>
     <div className="customerCard mt-16 flex gap-7"> 
-      <div className="customerCard1 w-[384px] h-[239px] noise-bg rounded-md m-auto">
-        <div className="flex  justify-start ml-4 mt-5 items-center">
+      <div className="customerCard1 w-[384px] h-[239px] noise-bg rounded-md m-auto p-3">
+        <div className="flex  justify-start  items-center">
           <div className="profilepic rounded-[50%] w-[60px] h-[60px] bg-amber-300 overflow-hidden"></div>
           <h1 className='text-2xl font-medium ml-2'>John D.</h1>
          </div>
-         <h1 className='ml-7 max-w-[500px] '>Using this AI-powered design assistant has completetly transformed the way i approach my projects. It's like having a professional desginer on call 24/7. Highly recommend it!</h1>
+         <h1 className='ml-7 max-w-[500px] '>Using this AI-powered design assistant has completetly transformed the way i approach my projects. It's like having a professional designer on call 24/7. Highly recommend it!</h1>
       </div>
+
+      
     </div>
   </>
 }
