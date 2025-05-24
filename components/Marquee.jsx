5835a03860a31a2528a18df869c52dd63d094eb2
morgankar <realrohankar03@gmail.com>
2025-05-24 18:23:15 +0530

    Marquee is now mobile responsive

--- components/Marquee.jsx
+++ components/Marquee.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Marquee = () => {
   return (
 
-    <div className='wrapper max-w-[1200px] mask-fade-right'>
+    <div className='wrapper sm:max-w-[1200px] mask-fade-right'>
         <div className="item item1"><img src="/marqueeImg/1.avif"/></div>
         <div className="item item2"><img src="/marqueeImg/2.avif"/></div>
         <div className="item item3"><img src="/marqueeImg/3.avif"/></div>
--- components/Section4.jsx
+++ components/Section4.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Section4 = () => {
   return (
     <div className='max-w-[1200px] m-auto rounded-3xl mt-18
-     noise-bg
-     overflow-hidden w-[90vw] h-[460px] flex items-center p-2'>
+     noise-bg overflow-hidden w-[90vw] h-[460px] flex items-center p-2'>
         <div className="txtblock ml-9 ">
             <h1 className='text-6xl font-medium w-[390px] '>Seamless Tool <span className='text-[#8b49e9]'>Integration</span></h1>
             <h1 className='w-[450px] mt-3'>HelixAI offers seamless integration with a variety of popular design and project management tools, ensuring a smooth and efficient workflow.</h1>
