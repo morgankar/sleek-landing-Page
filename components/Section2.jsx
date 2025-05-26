6bd885e8120ba9ce921cb751bee5f4dc09cee09f
morgankar <realrohankar03@gmail.com>
2025-05-26 16:47:26 +0530

    Section2 is now mobile responsive

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section2 = () => {
   return (
     <>
-        <div className="container sm:max-w-[1200px] sm:max-h[471px] m-auto flex sm:justify-between sm:items-center sm:mb-20 w-[90vw] flex-col-reverse sm:flex-col">
+        <div className="container sm:max-w-[1200px] sm:max-h[471px] m-auto flex sm:justify-between sm:items-center sm:mb-20 w-[90vw] flex-col-reverse sm:flex-col mb-20">
             <div className="textbox mt-14"> 
                 <h2 className='sm:text-5xl  font-bold sm:w-100 w-[90vw] text-2xl text-center'>High-Resolution Outputs</h2>
                 <h2 className='sm:w-[500px] mt-3 w-[90vw] text-center text-2xs'>Download any final designs in high-resolution formats suitable for print and digital use. Ensure your work looks professional and polished in any medium.</h2>
@@ -15,28 +15,29 @@ const Section2 = () => {
         </div>
 
 
-        <div className="container max-w-[1200px] max-h[471px] m-auto flex justify-between items-center  mb-20">
-            <img className="max-w-[380px] min-h-[471px] object-cover rounded-3xl" src="/img_3.avif" alt="Image"/>
-            <div className="textbox"> 
-                <h2 className='text-5xl font-bold w-100'>Real-Time Collaboration</h2>
-                <h2 className='w-[500px] mt-3'>Seamlessly collaborate with your team in real-time. Share ideas, give feedback, and make edits together, no matter where you are.</h2>
-                <div className="cta mt-6 text-sm font-bold ">
+        <div className="container sm:max-w-[1200px] sm:max-h[471px] m-auto flex sm:justify-between sm:items-center sm:mb-20 w-[90vw] flex-col-reverse sm:flex-col mb-20">
+             <div className="textbox mt-14"> 
+                <h2 className='sm:text-5xl  font-bold sm:w-100 w-[90vw] text-2xl text-center'>Real-Time Collaboration</h2>
+                <h2 className='sm:w-[500px] mt-3 w-[90vw] text-center text-2xs'>Seamlessly collaborate with your team in real-time. Share ideas, give feedback, and make edits together, no matter where you are.</h2>
+                <div className="cta mt-6 text-sm font-bold text-center sm:text-left">
                     <button className='bg-[#8f4ded] p-3 rounded-[10px]'>Remix Template</button>
                 </div>
             </div>
+            <img className="sm:max-w-[380px] sm:min-h-[471px] max-w-[65vw] m-auto sm:m-0 object-cover rounded-3xl" src="/img_3.avif" alt="Image"/>
            
         </div>
 
 
-        <div className="container max-w-[1200px] max-h[471px] m-auto flex justify-between items-center  mb-20">
-            <div className="textbox"> 
-                <h2 className='text-5xl font-bold w-100'>Advanced Scheduling Tools</h2>
-                <h2 className='w-[500px] mt-3'>Plan and schedule your design projects with ease. Use our scheduling tools to set deadlines, track milestones, and ensure timely delivery of your design work.</h2>
-                <div className="cta mt-6 text-sm font-bold ">
+        <div className="container sm:max-w-[1200px] sm:max-h[471px] m-auto flex sm:justify-between sm:items-center sm:mb-20 w-[90vw] flex-col-reverse sm:flex-col mb-20">
+            <div className="textbox mt-14"> 
+                <h2 className='sm:text-5xl  font-bold sm:w-100 w-[90vw] text-2xl text-center'>Advanced Scheduling Tools</h2>
+                <h2 className='sm:w-[500px] mt-3 w-[90vw] text-center text-2xs'>Plan and schedule your design projects with ease. Use our scheduling tools to set deadlines, track milestones, and ensure timely delivery of your design work.</h2>
+                <div className="cta mt-6 text-sm font-bold text-center sm:text-left">
                     <button className='bg-[#8f4ded] p-3 rounded-[10px]'>Remix Template</button>
                 </div>
             </div>
-            <img className="max-w-[380px] min-h-[471px] object-cover rounded-3xl" src="/img_4.avif" alt="Image"/>
+            <img className="sm:max-w-[380px] sm:min-h-[471px] max-w-[65vw] m-auto sm:m-0 object-cover rounded-3xl" src="/img_4.avif" alt="Image"/>
+            
         </div>
     </>
     
