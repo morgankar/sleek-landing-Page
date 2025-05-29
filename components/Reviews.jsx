954a223ba4e077ffef645dd281aaecdfebe4f097
morgankar <realrohankar03@gmail.com>
2025-05-29 13:28:14 +0530

    Marquee is now responsive

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Reviews = () => {
   return  <>
 
-    <div className='max-w-[1200px] m-auto mt-30 '>
-         <h1 className='text-6xl font-medium w-[520px] ml-9'>Customer <span className='text-[#8b49e9]'>Success</span> Stories</h1>
-         <h1 className='w-[650px] mt-3 ml-9'>Discover how our platform has helped businesses create outstanding content effortlessly. Hear directly from our users about their success and satisfaction.</h1>
+    <div className=' border-2 sm:max-w-[1200px] max-w-[90vw] m-auto mt-20 '>
+         <h1 className='sm:text-6xl text-4xl font-medium sm:w-[520px] w-[90vw] sm:ml-9 '>Customer <span className='text-[#8b49e9]'>Success</span> Stories</h1>
+         <h1 className='sm:w-[650px] mt-3 sm:ml-9 w-[90vw] '>Discover how our platform has helped businesses create outstanding content effortlessly. Hear directly from our users about their success and satisfaction.</h1>
     </div>
+
+
     <div className="customerCard mt-16 flex gap-7"> 
       <div className="customerCard1 w-[384px] h-[239px] noise-bg rounded-md m-auto p-3">
         <div className="flex  justify-start  items-center">
--- components/Section4.jsx
+++ components/Section4.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Section4 = () => {
   return (
     <div className='sm:max-w-[1200px] max-w-[90vw] m-auto rounded-3xl mt-18
-     noise-bg overflow-hidden w-[90vw] h-[460px] flex sm:items-center p-2 border-2'>
+     noise-bg overflow-hidden w-[90vw] h-[460px] flex sm:items-center p-4 '>  
         <div className="txtblock sm:ml-9 m-auto ">
             <h1 className='sm:text-6xl text-5xl font-medium sm:w-[390px] w-[90vw] '>Seamless Tool <span className='text-[#8b49e9]'>Integration</span></h1>
             <h1 className='sm:w-[450px] w-[80vw] mt-3'>HelixAI offers seamless integration with a variety of popular design and project management tools, ensuring a smooth and efficient workflow.</h1>
         </div>
-        <div className="sm:w-[40vw] sm:h-[450px] mx-auto  w-[20vw] h-[20vh]">
-          <div className="itemIcon item1"><img src="/marqueeImg/a.avif"/></div>
-          <div className="itemIcon item2"><img src="/marqueeImg/b.avif"/></div>
-          <div className="itemIcon item3"><img src="/marqueeImg/c.avif"/></div>
-          <div className="itemIcon item4"><img src="/marqueeImg/d.avif"/></div>
-          <div className="itemIcon item5"><img src="/marqueeImg/e.avif"/></div>
+        <div className="sm:w-[40vw] sm:h-[450px] mx-auto  relative">
+          <div className="itemIcon item1 "><img src="/marqueeImg/a.avif"/></div>
+          <div className="itemIcon item2 absolute"><img src="/marqueeImg/b.avif"/></div>
+          <div className="itemIcon item3 absolute"><img src="/marqueeImg/c.avif"/></div>
+          <div className="itemIcon item4 absolute"><img src="/marqueeImg/d.avif"/></div>
+          <div className="itemIcon item5 absolute"><img src="/marqueeImg/e.avif"/></div>
          
         </div>
      </div>
