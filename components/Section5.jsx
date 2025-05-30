0e4b56a1058a75303c0b25fb3bf8a3e2f6c88d0e
morgankar <realrohankar03@gmail.com>
2025-05-30 18:42:57 +0530

    Section 5 mobile responsive

--- components/Section5.jsx
+++ components/Section5.jsx
@@ -7,14 +7,56 @@ const Section5 = () => {
         <h1 className='mt-4 sm:max-w-[40vw]'>Dive into our blog for the latest trends, tips, and insights in the world of design and AI technology. Whether you're looking for inspiration, tutorials, or industry news, our articles are crafted to keep you informed and inspired.</h1>
 
         <div className="three flex flex-col sm:flex-row sm:gap-6 mt-10 items-center justify-center">
-            <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden"></div>
-            <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden"></div>
-            <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden"></div>
+
+            <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden flex flex-col">
+                <div className='h-[70%] overflow-hidden'>
+                    <img className='scale-120' src="imgA.avif" alt="img"/>
+                </div>
+                <div className="textbox p-2 h-[50%]">
+                    <div className='flex justify-between mt-3'>
+                        <h1>Apr 8,2022</h1>
+                        <h1>6 min read</h1>
+                    </div>
+                    <h1 className='text-2xl font-medium mt-5  '>Starting a Growing a Career in Web Design</h1>
+                </div>
+            </div>
+
+            {/* Card2 */}
+              <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden flex flex-col">
+                <div className='h-[70%] overflow-hidden'>
+                    <img className='scale-120' src="imgB.avif" alt="img"/>
+                </div>
+                <div className="textbox p-2 h-[50%]">
+                    <div className='flex justify-between mt-3'>
+                        <h1>Mar 15,2022</h1>
+                        <h1>5 min read</h1>
+                    </div>
+                    <h1 className='text-2xl font-medium mt-5 '>Create a Landing Page that performs Great</h1>
+                </div>
+            </div>
+
+            {/* card3 */}
+
+              <div className="mt-4 sm:mt-0 noise-bg h-[350px] w-[90vw] rounded-2xl overflow-hidden flex flex-col">
+                <div className='h-[70%] overflow-hidden'>
+                    <img className='scale-120' src="imgC.avif" alt="img"/>
+                </div>
+                <div className="textbox p-2 h-[50%]">
+                    <div className='flex justify-between mt-3'>
+                        <h1>Feb 28,2022</h1>
+                        <h1>7 min read</h1>
+                    </div>
+                    <h1 className='text-2xl font-medium mt-5 '>How Can Designers Prepare for the future?</h1>
+                </div>
+            </div>
+
         </div>
 
          <div className="cta mt-6 text-sm font-bold text-center ">
             <button className='bg-[#8f4ded] p-3 rounded-[10px] sm:max-w-[230px]'>Read More</button>
         </div>
+        
+
     </div>
   )
 }
