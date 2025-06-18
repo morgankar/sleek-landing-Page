58bc3d5abd61726b423439e613a78399a98df32c
morgankar <realrohankar03@gmail.com>
2025-06-18 20:13:16 +0530

    added hamburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <>
       <div className="navbar 
@@ -17,15 +19,72 @@ const Navbar = () => {
         <div className="rightNav flex gap-5 items-center">
           <h1 className='font-semibold opacity-0 sm:opacity-100'>More Designs</h1>
           
-          <button className='hamburg noise-bg px-5 rounded-xl py-2 w-auto font-semibold flex items-center justify-center '> 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" /></svg>
+          <button 
+          onClick={() => setIsOpen(!isOpen)}
+          
+          className='hamburg noise-bg-pointer cursor-pointer px-5 rounded-xl py-2 w-auto font-semibold flex items-center justify-center '> 
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-6 h-6"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" /></svg>
           </button>
         </div>
         
 
+      </div>
+
+
+      {/* Mobile Menu */}
+      <div className={`
+      ${isOpen ? 'block':'hidden'}
+        
+        bg-gray-700/60 rounded-2xl  text-shadow-white absolute z-900 backdrop-blur-[8px] right-0 p-5  h-[290px] text-white  top-[100px] left-0 w-[80vw] px-8 py-4 transition-all duration-300 m-auto`}>
+        <ul>
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              Home
+            </a>
+          </li>
+
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              How it Works
+            </a>
+          </li>
+
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              Features
+            </a>
+          </li>
+
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              Pricing
+            </a>
+          </li>
+
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              Blog
+            </a>
+          </li>
+
+          <li className='mb-3'>
+            <a href="#" className="text-white hover:text-gray-300">
+              Faqs
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   )
 }
 
 export default Navbar
+
+//Make useState use to track changes of the states of each variable you wanna record. 
+// it basically can be used to implement certain logic based on what is the state of the variable.
