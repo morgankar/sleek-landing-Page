0d22ef724a2254633231d6e3253955dd8d6031c5
morgankar <realrohankar03@gmail.com>
2025-06-18 17:37:59 +0530

    Fixed arrow direction

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -59,7 +59,7 @@ const Faq = () => {
                             {item.title} 
                             <motion.svg 
                             initial = {{rotate: 180}}
-                            animate={{rotate: activeId === index ? 180:0}}
+                            animate={{rotate: activeId === index ? 0:180}}
                             transition={{duration: 0.5}}
                             className="w-5 h-5 mr-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9999 10.8284L7.0502 15.7782L5.63599 14.364L11.9999 8L18.3639 14.364L16.9497 15.7782L11.9999 10.8284Z"></path></motion.svg>
                             </div>
